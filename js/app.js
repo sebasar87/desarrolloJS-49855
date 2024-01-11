d71a6f5923c0bd52214e38654f3539f5ecfb3cd7
Sebastian Arenas <arenas87sebastian@gmail.com>
2024-01-10 23:58:06 -0300

    se creo la idea del proyecto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
-console.log ("buenos dias");
+//console.log ("buenos dias");
+
+//console.log ("buenos dias, su monto es ");
+let opcion = "si";
+
+do {  
+
+    let valor = parseFloat(prompt ("Buenos dias calcularemos el interes compuesto de su plazo fijo ingrese monto, solo numeros"));
+
+    const porcent = parseFloat(prompt ("ingrese el porcentaje que pagara el banco, solo numero"));  
+
+    let meses = parseInt(prompt ("Ingrese la cantidad de meses que establecera el plazo fijo, solo numero"));
+
+    let conver = parseFloat(porcent/100);
+
+
+    for(let i = 0 ; i < meses ; i++){
+        valor = valor + (valor*conver);
+    }
+
+
+    console.log ("Al finalizar el tiempo usted recibira " + valor);
+
+    opcion = prompt("desea realizar otra prueba. escriba 'si' para continuar o 'no' para salir");
+
+    console.log(opcion);
+
+} while (opcion != 'no');
+
+alert ("gracias por utilizar nuestro servicios, vuelva prontos");
