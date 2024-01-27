a999ebdcd3fcf0be2b5970ed0e84857e909ecd38
Sebastian Arenas <arenas87sebastian@gmail.com>
2024-01-26 23:40:08 -0300

    se agregaron funciones y se hicieron correciones

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ class Productos{
         this.cantidad = cantidad;
     }
 //metodos para obtener losatributos del objeto producto
-    nombreProducto(){
+/*    nombreProducto(){
         return this.nombre;
     }
     precioProducto(){
@@ -20,83 +20,105 @@ class Productos{
     }
     cantidadProducto(){
         return this.cantidad;
-    }
-//metodos para realizar la modificacion de los atributos del producto
-    nombreModificar(index,nombre){
-        this.nombre[index] = nombre;
-        return this.nombre;
-    }
-    precioModificar(index,precio){
-        this.precio[index] = precio;
-        return this.nombre; 
-    }
-    cantidadModificar(index,cantidad){
-        this.cantidad[index] = cantidad;
-        return this.nombre;
-    }
-    /*buscarProducto(lista,nombre){
-        let index = lista.findIndex(Producto => Producto.nombre === nombre);
-        return index 
-        if(index !== ""){
-            return index;
-        }else{
-            return "el producto no existe"
-        }
     }*/
-
 }
-
+//funcion para buscar el index del producto en el array
 function buscar(nombre){
     return listaProductos.findIndex((producto)=> producto.nombre === nombre);
 }
-
+//funcion para cargar unproducto nuevo
 function cargar (){
     let nombre = prompt("Ingrese nombre del producto").toUpperCase().trim();
+    let ind = buscar(nombre);
+    if(ind == -1){
     let precio = parseFloat(prompt("Ingrese precio del producto"));
     let cantidad = parseInt(prompt("Ingrese la cantidad disponible del producto"));
     const Producto = new Productos(nombre,precio,cantidad)
-    return Producto;
+    listaProductos.push(Producto);
+    }else{
+        alert("El producto ya existe")
+    }
+}
+//funcion que permite modificar un producto
+function modificar(){
+    let ind = buscar(prompt("ingrese el nombre del producto a modificar").toUpperCase().trim());
+    if(ind != -1){ 
+    listaProductos[ind].nombre = prompt("Ingrese el nuevo nombre").toUpperCase().trim();
+    listaProductos[ind].precio = prompt("ingrese nuevo precio");
+    listaProductos[ind].cantidad = prompt("ingrese nueva cantidad");
+    return ind;
+    }else{
+        alert("El producto no existe");
+    }
+}
+//funcion para eliminar un producto
+function eliminar(){
+    let ind = buscar(prompt("ingrese el nombre del producto a eliminar").toUpperCase().trim());
+    listaProductos.splice(ind,1);
 }
 
-const Producto = new Productos("cafe",56,90);
+//funcion decimal que convierte el numero de porcentaje sin el % en decimal
+const decimal = (p) => {return p/100}
+//funcion que muestra cuanto costarian los productos con un aumento
+function aumentar(){
+    let evaluar = "false";
+    let ingreso = "";
+    do {
+        ingreso = parseFloat(prompt("Ingrese el porcentaje sin '%' para ver como quedarian los precios con un aumento"));
+        if (!isNaN(ingreso)){
+            evaluar = "true"
+        } else{
+            alert ("no es un numero intente de nuevo");
+        }
+    }while (evaluar != "true");   
+    
+    let porcen = decimal(ingreso);
+
+    const nuevalist = listaProductos.map((x) => {
+        return {
+            nombre: x.nombre, precio:(x.precio + (x.precio * porcen)), cantidad: x.cantidad }} );
+
+    verProd(nuevalist);
+
+}
+//funcion que muestra la lista de los productos
+function verProd(lista){    
+    console.table(lista);
+}
+
+/*const Producto = new Productos("CAFE",56,90);
 listaProductos.push(Producto);
 let numero=listaProductos.length;
 console.log("valor numero cantidad de productos cargados " + numero);
 console.log("el producto cargado es " + listaProductos[numero-1].nombre + ", su precio es " + listaProductos[numero-1].precio + ", la cantidad es " + listaProductos[numero-1].cantidad);
-
+*/
 do{
 saludo ();
-opcion = prompt("Ingrese 1 para agregar producto\nIngrese 2 para modificar producto\nIngrese 3 para eliminar producto\nIngrese 4 para ver todos los productos\nIngrese 5 para salir");
-//opcion = confirm("desea salir?");
-//console.log ("opcion es " + opcion);
+opcion = prompt("Ingrese 1 para agregar producto\nIngrese 2 para modificar producto\nIngrese 3 para eliminar producto\nIngrese 4 para ver todos los productos\nIngrese 5 para calcular el aumento en los precios\nIngrese 6 para salir");
 switch(opcion){
     case "1":
         alert ("Ingrese los datos del producto");
-        listaProductos.push(cargar ())
+        //listaProductos.push(cargar ());
+        cargar ()
         let numero=listaProductos.length;
         console.log("valor numero cantidad de productos cargados " + numero);
         console.log("el producto cargado es " + listaProductos[numero-1].nombre + ", su precio es " + listaProductos[numero-1].precio + ", la cantidad es " + listaProductos[numero-1].cantidad);
         break;
     case "2":
-        alert ("Ingrese que producto modificar");
-/* 
-        ver = prompt("ingrese nombre del producto")
-        let ind = Productos.buscarProducto(listaProductos,ver)
-        if(ind){
-            console.log("el producto es " + ver);
-        }else{
-            alert("fallo " + ver);
-        } */
-        let ind = buscar(prompt("ingrese el nombre del producto").toUpperCase().trim());
-        console.log("la ubicacion es " + ind);
+        let ind = modificar();
+        verProd(listaProductos);
         break;
     case "3":
-        alert ("Ingrese que producto eliminar");
+        eliminar();
+        verProd(listaProductos);
         break;
     case "4":
-        console.table(listaProductos);
+        verProd(listaProductos);
         break;
     case "5":
+        aumentar();
+        break;
+    case "6":
         opcion = confirm("desea salir?");
         break; 
 }
